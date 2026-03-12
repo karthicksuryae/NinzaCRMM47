8d2108cedfc09288573c078cfa44b9283c1b14a0
karthicksuryae <karthicksurya16@gmail.com>
2026-03-12 07:51:41 +0530

    first time commiting

---
+++ src/test/java/Practice/Demo.java
@@ -0,0 +1,13 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+	
+	@Test
+	public void carTest()
+	{
+		System.out.println("BMW");
+	}
+
+}
---
+++ src/test/java/Practice/Demo1.java
@@ -0,0 +1,15 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class Demo1 {
+	
+	
+	@Test(groups = "smoke")
+	public void bikeTest()
+	
+	{
+		System.out.println("RE");
+	}
+
+}
